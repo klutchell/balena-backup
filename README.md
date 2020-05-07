c7c8a9bdb62e5212d3034c8fe75bd2241306ca9f
Kyle Harding <kmharding@pm.me>
2020-05-07 09:43:44 -0400

    add readme and cleanup backup script

---
+++ README.md
@@ -0,0 +1,105 @@
+# balena-backup
+
+non-interactive backup utility for balenaCloud managed devices
+
+## Requirements
+
+- one or more devices managed via [balenaCloud](https://www.balena.io/cloud/)
+- linux workstation or backup server with [balena CLI](https://www.balena.io/docs/reference/balena-cli/)
+
+## Getting Started
+
+Install the [balena CLI](https://www.balena.io/docs/reference/balena-cli/) and authenticate with balenaCloud.
+
+```bash
+balena login
+```
+
+Make sure you have root ssh access to the HostOS of your balenaCloud devices.
+
+- <https://www.balena.io/docs/learn/manage/ssh-access/#using-a-standalone-ssh-client>
+
+> If you prefer to use a standalone SSH client to connect to the device, the SSH server on a device listens on TCP port 22222. While development images have passwordless root access enabled, production images require an SSH key to be added to the config.json file.
+
+I personally added SSH keys to my Production images via the [configizer project](https://github.com/balena-io-playground/configizer). Other options are outlined here.
+
+- <https://www.balena.io/docs/reference/OS/configuration>
+
+## Deployment
+
+Once your SSH keys are up, deployment is carried out by downloading or cloning the project and running it while passing in a local destination directory of your choosing.
+
+## Usage
+
+This utility will perform the following tasks in order for each balenaCloud managed device.
+
+1. parse a list of any mysql services on device
+2. for each service from the previous step dump the mysql database to a file
+3. parse a list of all non-dangling persistent volumes
+4. start an rsync backup container and mount all volumes from the previous step
+5. sleep container for x seconds while remaining steps are performed
+6. disconnect from the device and return to workstation shell
+7. start a new tunnel from localhost port 1234 to remote device 22222 (ssh)
+8. use rsync to mirror all sources (volumes) from the rsync backup container to a local directory
+
+### configuration
+
+Edit the values at the top of `backup.sh` as desired, or export them in the environment beforehand.
+
+### run manually on demand
+
+Note the example destination dir of `~/balenaCloud`. Subfolders will be created for each device UUID.
+
+```bash
+export MYSQL_ROOT_PASSWORD=********
+./backup.sh ~/balenaCloud
+```
+
+### run daily on a schedule with cron
+
+Note the absolute path to the backup utility and backup destination. Cron often does not have access to your full environment or PATH.
+
+`crontab -e`
+
+```bash
+# For example, you can run a backup of all your balenaCloud devices at 5 a.m every week with:
+0 5 * * 1 /home/klutchell/workspace/balena-backups/backup.sh /home/klutchell/balenaCloud
+```
+
+For more info on cron usage see <https://en.wikipedia.org/wiki/Cron>.
+
+### Add to an existing rsnapshot backup configuration
+
+`nano /etc/rsnapshot.conf`
+
+```bash
+# run balenaCloud backup script to a local directory
+backup_exec	/home/klutchell/workspace/balena-backups/backup.sh /var/balenaCloud
+
+# include local directory in rsnapshot backups
+backup	/var/balenaCloud/	balenaCloud/
+```
+
+For more info on rsnapshot usage see <https://rsnapshot.org/>.
+
+## Contributing
+
+Please open an issue or submit a pull request with any features, fixes, or changes.
+
+## Author
+
+Kyle Harding <https://klutchell.dev>
+
+[Buy me a beer](https://kyles-tip-jar.myshopify.com/cart/31356319498262:1?channel=buy_button)
+
+[Buy me a craft beer](https://kyles-tip-jar.myshopify.com/cart/31356317859862:1?channel=buy_button)
+
+## Acknowledgments
+
+- <https://github.com/balena-io/configizer>
+- <https://www.balena.io/docs/learn/manage/ssh-access/#using-balena-ssh-from-the-cli>
+- <https://forums.balena.io/t/rsync-over-balena-ssh-tunnel/6228/4>
+
+## License
+
+[MIT License](./LICENSE)
--- backup.sh
+++ backup.sh
@@ -1,56 +1,89 @@
 #!/bin/bash
 
-set -ex
+set -eu
 
-# MYSQL_ROOT_PASSWORD must be set
-[ -n "${MYSQL_ROOT_PASSWORD}" ] || exit 1
+# make sure we are logged in via balena cli
+balena whoami
 
-# 1. for each device on balenaCloud account
-# 2. log in to Host via balena ssh
-# 3. print uptime
-# 4. for each service named 'mariadb'
-# 5. dump all mysql databases to /var/lib/mysql/dump.sql
-# 6. repeat step 5 for each service named 'mariadb'
-# 7. stop and remove any existing containers named 'vol_backup'
-# 8. create a list of all non-dangling volumes
-# 9. start and alpine container and mount all volumes from step 8
-# 10. install rsync in the alpine container and sleep for next steps (5 min)
-# 11. logoff the Host
-# 12. kill any existing balena tunnel process
-# 13. start a new tunnel from localhost:1234 to remote Host :22222 (ssh)
-# 14. customize rsync shell command to exec into alpine container on remote Host
-# 15. run rsync to backup all sources (volumes) from the alpine container to a local directory
-# 16. kill any existing balena tunnel process
-# 17. the alpine container should die and clean up 5 minutes after starting
-# 18. repeat steps 2-17 for each device on balenaCloud account
+# change this value as needed or pass it as an arguement when calling the script
+BACKUP_DEST="${1:-${HOME}/balenaCloud}"
 
-for device in $(balena devices | awk '{print $2}' | grep -v UUID)
+# change these values as needed or export them in the environment beforehand
+BALENA_DEVICES="${BALENA_DEVICES:-$(balena devices | awk '{print $2}' | grep -v UUID)}"
+
+# change these values as needed or export them in the environment beforehand
+MYSQL_SERVICES="${MYSQL_SERVICES:-mysql mariadb db}"
+MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD:-}"
+MYSQL_DUMP_FILE="${MYSQL_DUMP_FILE:-/var/lib/mysql/dump.sql}"
+
+# seconds until rsync container is automatically removed (5 min)
+# increase this value if it takes longer to backup one of your devices
+RSYNC_CONTAINER_WAIT=300
+
+# you normally shouldn't need to change these
+RSYNC_CONTAINER_NAME=rsync_backup
+RSYNC_LOCAL_PORT=4321
+
+# definitely don't change this function
+RSYNC_RSH=$(mktemp)
+cat > "${RSYNC_RSH}" <<- EOF
+#!/bin/bash
+new_args=()
+for arg in "\$@"
 do
+    if [ "\${arg}" = "${RSYNC_CONTAINER_NAME}" ]
+    then
+        new_args+=("-p ${RSYNC_LOCAL_PORT} root@127.0.0.1" balena exec -i "${RSYNC_CONTAINER_NAME}")
+    else
+        new_args+=("\${arg}")
+    fi
+done
+ssh \${new_args[@]}
+EOF
+
+chmod a+x "${RSYNC_RSH}"
+export RSYNC_RSH
+
+### END DEFINITIONS - START TASKS ###
+
+for device in ${BALENA_DEVICES}
+do
+    balena device "${device}"
     cat << EOF | balena ssh "${device}" --tty
-set -ex
 uptime
 
-for db in \$(balena container ls -q -f label=io.balena.service-name=mariadb)
+set -eu
+
+for name in ${MYSQL_SERVICES}
 do
-    balena exec \${db} sh -c 'mysqldump -v -A -uroot -p${MYSQL_ROOT_PASSWORD} > /var/lib/mysql/dump.sql'
+    for db in \$(balena container ls -q -f label=io.balena.service-name=\${name})
+    do
+        echo "running mysqldump for service \${name}..."
+        balena exec \${db} sh -c 'mysqldump -v -A -uroot -p${MYSQL_ROOT_PASSWORD} > ${MYSQL_DUMP_FILE}'
+    done
 done
 
-balena stop vol_backup || true
-balena rm vol_backup || true
+balena stop ${RSYNC_CONTAINER_NAME} &>/dev/null || true
+balena rm ${RSYNC_CONTAINER_NAME} &>/dev/null || true
 
-args="--rm -d --name vol_backup"
+args="--rm -d --name ${RSYNC_CONTAINER_NAME}"
 for vol in \$(balena volume ls -q -f dangling=false)
 do
+    echo "adding volume \${vol} to backup..."
     args="\${args} -v \${vol}:/sources/\${vol}:ro"
 done
 
-balena run \${args} alpine sh -c 'apk add --no-cache rsync && sleep 300'
+echo "starting ${RSYNC_CONTAINER_NAME} container with ${RSYNC_CONTAINER_WAIT}s timeout..."
+balena run \${args} alpine sh -c 'apk add --no-cache rsync && sleep ${RSYNC_CONTAINER_WAIT}'
 exit
 EOF
-    kill "$(pidof balena)" || true
-    (balena tunnel "${device}" -p 22222:4321 &
+
+    kill "$(pidof balena)" 2>/dev/null || true
+    (balena tunnel "${device}" -p 22222:${RSYNC_LOCAL_PORT} &
     sleep 5)
-    export RSYNC_RSH="./rsync-shell.sh" && chmod +x "${RSYNC_RSH}"
-    rsync -avz vol_backup:/sources/ ./backups/"${device}"/
+
+    mkdir -p "${BACKUP_DEST}/${device}" 2>/dev/null || true
+    echo "starting rsync backup to ${BACKUP_DEST}/${device}..."
+    rsync -avz ${RSYNC_CONTAINER_NAME}:/sources/ "${BACKUP_DEST}"/"${device}"/ --delete
     kill "$(pidof balena)"
 done

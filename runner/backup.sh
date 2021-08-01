4389ad70a157a2d5debe50815cf965214dbc67e0
Kyle Harding <kyle@balena.io>
2021-08-01 11:53:29 -0400

    Refactor rsync-shell commands
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- runner/backup.sh
+++ runner/backup.sh
@@ -2,33 +2,19 @@
 
 set -eu
 
+uuid="${1}"
+backup_id="${2}"
+
 # shellcheck disable=SC1091
 source /usr/src/app/ssh-agent.sh
 
 # shellcheck disable=SC1091
 source /usr/src/app/balena-api.sh
 
-uuid="${1}"
-backup_id="${2}"
 username="$(get_username)"
 
-RSYNC_RSH=$(mktemp)
-cat > "${RSYNC_RSH}" <<- EOF
-#!/bin/bash
-new_args=()
-for arg in "\$@"
-do
-    if [ "\${arg}" = "${uuid}" ]
-    then
-        new_args+=("${username}@ssh.balena-devices.com host -s ${uuid}")
-    else
-        new_args+=("\${arg}")
-    fi
-done
-ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \${new_args[@]}
-EOF
-chmod a+x "${RSYNC_RSH}"
-export RSYNC_RSH
+# shellcheck disable=SC1091
+source /usr/src/app/rsync-shell.sh "${uuid}" "${username}"
 
 echo "Backing up ${uuid} as ${backup_id}..."
 backup_id="${backup_id//[^[:alnum:]_-]/}"
---
+++ runner/rsync-shell.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+uuid="${1}"
+username="${2}"
+
+RSYNC_RSH=$(mktemp)
+cat > "${RSYNC_RSH}" <<- EOF
+#!/bin/bash
+new_args=()
+for arg in "\$@"
+do
+    if [ "\${arg}" = "${uuid}" ]
+    then
+        new_args+=("${username}@ssh.balena-devices.com host -s ${uuid}")
+    else
+        new_args+=("\${arg}")
+    fi
+done
+ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \${new_args[@]}
+EOF
+chmod a+x "${RSYNC_RSH}"
+export RSYNC_RSH
+
+remote_ssh_cmd() {
+    ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 \
+        "${username}@ssh.balena-devices.com" host -s "${uuid}" "$@"
+}

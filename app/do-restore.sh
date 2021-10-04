664bf69b62563c0815199f14f967bcbb6334d765
Kyle Harding <kyle@balena.io>
2021-10-04 10:43:05 -0400

    Remove backup name env var and use type and path to guarantee uniqueness
    Change-type: minor
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- app/do-restore.sh
+++ app/do-restore.sh
@@ -10,16 +10,23 @@ source /usr/src/app/functions.sh
 
 backup_id="$(sanitize "${1}")"
 uuid="$(sanitize "${2}")"
-backend="${3:-$BACKEND_NAME}"
+backend_type="${3:-$BACKEND_TYPE}"
+backend_path="${4:-$BACKEND_PATH}"
 
-config="$(backend_config "${backend}")"
 cache="${CACHE_ROOT}/${backup_id}"
+
+backend_id="$(get_backend_id "${backend_type}" "${backend_path}")"
+
+# set_backend_config "${backend_id}" "${backend_type}" "${backend_path}"
+# set_location_config "${backup_id}" "${uuid}" "${cache}" "${backend_id}"
+
+config="$(get_backend_config "${backend_id}")"
 
 /usr/bin/autorestic --ci --verbose --config "${config}" check
 
 /usr/bin/autorestic --verbose --config "${config}" restore \
     --location "${backup_id}" \
-    --from "${backend}" \
+    --from "${backend_id}" \
     --to "${cache}" \
     --force
 
--- app/functions.sh
+++ app/functions.sh
@@ -3,7 +3,7 @@
 set -eu
 
 sanitize () {
-    echo "${1//[^[:alnum:]_-]/}"
+    echo "${1//[^[:alnum:]_-]/_}"
 }
 
 # https://github.com/Jeff-Russ/bash-boolean-helpers
@@ -22,64 +22,151 @@ truthy () {
 	return $ret_code
 }
 
-backend_config () {
-    echo "/config/$(sanitize "${1}").yml"
+# returns a unique backend id for the provided type/path combinations
+# 1. searches all config yml files existing backends matching this type/path
+# 2. failing that, finds the first unused id in the format {type}-#
+get_backend_id() {
+
+    local backend_type
+    local backend_path
+
+    backend_type="${1}"
+    backend_path="${2}"
+
+    local backend_id
+
+    local _type
+    local _path
+
+    for backend_id in $(yq ea -N '.backends | keys' /config/*.yml 2>/dev/null | awk '{print $NF}')
+    do
+        _type="$(get_backend_value "${backend_id}" "type")"
+        _path="$(get_backend_value "${backend_id}" "path")"
+
+        # this backend id exists but does not match our type/path, skip
+        [ "${backend_type}" = "${_type}" ] || continue
+        [ "${backend_path}" = "${_path}" ] || continue
+
+        # this backend id exists and matches our type/path, break
+        echo "${backend_id}"
+        return
+    done
+
+    for i in {1..99}
+    do
+        backend_id="${backend_type}-${i}"
+
+        _type="$(get_backend_value "${backend_id}" "type")"
+        _path="$(get_backend_value "${backend_id}" "path")"
+   
+        # this backend id was not found by the above loop so it's not for us
+        [ -n "${_type}" ] && [ -n "${_path}" ] && continue
+
+        # this backend id appears unused, break
+        echo "${backend_id}"
+        return
+    done
 }
 
-init_backend() {
+get_backend_value() {
 
-    local backend
-    local config
+    local backend_id
+    local value
+    local ret
 
-    backend="${1}"
-    config="$(backend_config "${backend}")"
+    backend_id="${1}"
+    value="${2}"
+
+    # yq ea -N ".backends.${backend_id}.${value}" /config/*.yml 2>/dev/null || true
+    for ret in $(yq ea -N ".backends.${backend_id}.${value}" /config/*.yml 2>/dev/null)
+    do
+        if [ "${ret}" != null ]
+        then
+            echo "${ret}"
+            return
+        fi
+    done
+
+    echo ""
+}
+
+get_backend_config() {
+
+    local backend_id
+
+    backend_id="${1}"
+
+    local config="/config/${backend_id}.yml"
 
     [ -f "${config}" ] || echo "---" > "${config}"
 
-    echo "Generating backend config for ${backend}..."
-
-    yq e "
-        .backends.${backend}.type = strenv(BACKEND_TYPE) |
-        .backends.${backend}.path = strenv(BACKEND_PATH)
-        " -i "${config}"
+    echo "${config}"
 }
 
-init_location() {
+set_backend_config() {
 
-    local backup_id
-    local uuid
-    local backend
+    local backend_id
+    local backend_type
+    local backend_path
+
+    backend_id="${1}"
+    backend_type="${2}"
+    backend_path="${3}"
+
     local config
+
+    config="$(get_backend_config "${backend_id}")"
+
+    local backend_key
+
+    backend_key="$(get_backend_value "${backend_id}" "key")"
+
+    echo "Configuring backend '${backend_id}'..."
+
+    if [ -n "${backend_key}" ]
+    then
+        yq e "
+        .backends.${backend_id}.type = \"${backend_type}\" |
+        .backends.${backend_id}.path = \"${backend_path}\" |
+        .backends.${backend_id}.key = \"${backend_key}\"
+        " -i "${config}"
+    else
+        yq e "
+        .backends.${backend_id}.type = \"${backend_type}\" |
+        .backends.${backend_id}.path = \"${backend_path}\"
+        " -i "${config}"
+    fi
+}
+
+set_location_config() {
+
+    local backend_id
+    local uuid
     local cache
+    local backup_id
 
     backup_id="$(sanitize "${1}")"
     uuid="$(sanitize "${2}")"
-    backend="${3}"
-    cache="${4}"
+    cache="${3}"
+    backend_id="${4}"
 
-    config="$(backend_config "${backend}")"
-
-    mkdir -p "${cache}"
+    local config
+    
+    config="$(get_backend_config "${backend_id}")"
 
     local before="/usr/src/app/sync-cache.sh ${uuid} ${cache}"
     local after="date"
-    local success="echo '${backup_id} succeeded'"
-    local failure="echo '${backup_id} failed'"
+    local success="echo 'success for ${backup_id}'"
+    local failure="echo 'failure for ${backup_id}'"
 
-    echo "Generating location config for ${backup_id}..."
+    echo "Configuring location '${backup_id}'..."
 
-    before="${before}" \
-    after="${after}" \
-    success="${success}" \
-    failure="${failure}" \
-    backend="${backend}" \
-    cache="${cache}" \
     yq e "
-        .locations.${backup_id}.from = strenv(cache) |
-        .locations.${backup_id}.to = strenv(backend) |
-        .locations.${backup_id}.hooks.before = [strenv(before)] |
-        .locations.${backup_id}.hooks.after = [strenv(after)] |
-        .locations.${backup_id}.hooks.success = [strenv(success)] |
-        .locations.${backup_id}.hooks.failure = [strenv(failure)]
-        " -i "${config}"
+    .locations.${backup_id}.from = \"${cache}\" |
+    .locations.${backup_id}.to = \"${backend_id}\" |
+    .locations.${backup_id}.hooks.before = [\"${before}\"] |
+    .locations.${backup_id}.hooks.after = [\"${after}\"] |
+    .locations.${backup_id}.hooks.success = [\"${success}\"] |
+    .locations.${backup_id}.hooks.failure = [\"${failure}\"]
+    " -i "${config}"
 }

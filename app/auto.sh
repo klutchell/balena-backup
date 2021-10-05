a4d4d51473612956e09d3864678a4430d5786d81
Kyle Harding <kyle@balena.io>
2021-10-05 11:47:55 -0400

    Prevent self-backup
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- app/auto.sh
+++ app/auto.sh
@@ -10,6 +10,8 @@ source /usr/src/app/balena-api.sh
 
 for uuid in $(get_online_uuids_with_tag_key backup_tags)
 do
+    [ "${uuid}" = "${BALENA_DEVICE_UUID:-}" ] && continue
+    
     backup_tags="$(get_uuid_tag_value "${uuid}" backup_tags)"
 
     DRY_RUN="${DRY_RUN:-}" /usr/src/app/do-backup.sh "${uuid}" "${backup_tags}" "${RESTIC_REPOSITORY}"
